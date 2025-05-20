f5a57cc1ef4f515ea758cd82bfb4a2e34d12a512
Linke <lilinke99@gmail.com>
2025-05-20 20:28:57 +0000

    update new trace method

---
+++ Function_instrument/trace.c
@@ -0,0 +1,16 @@
+#ifdef __APPLE__
+  #define _DARWIN_C_SOURCE
+#endif
+#define _GNU_SOURCE 
+#include <stdio.h>
+#include <dlfcn.h>
+#include <stdint.h>
+
+void __cyg_profile_func_enter(void *func, void *caller) {
+    Dl_info info;
+    if (dladdr(func, &info)) {
+      uintptr_t offset = (uintptr_t)func - (uintptr_t)info.dli_fbase;
+      printf("offset: %ld\n", offset);
+    }
+    fflush(stdout);
+}
