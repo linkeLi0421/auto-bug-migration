c0edcfa85e614bb0f6a8b9121ffb7531f5263f53
Linke <lilinke99@gmail.com>
2025-10-12 17:43:21 +0000

    Fix an issue lead to sometimes only one funciton in trace printed

--- Function_instrument/trace.c
+++ Function_instrument/trace.c
@@ -6,12 +6,19 @@
 #include <dlfcn.h>
 #include <stdint.h>
 
+static FILE *trace_fp = NULL;
+
 void __cyg_profile_func_enter(void *func, void *caller) {
+    if (!trace_fp) {
+        trace_fp = fopen("/tmp/trace.txt", "w");
+        if (!trace_fp) trace_fp = stderr;
+    }
+    
     Dl_info info;
     if (dladdr(func, &info)) {
       uintptr_t offset = (uintptr_t)func - (uintptr_t)info.dli_fbase;
       uintptr_t caller_offset = (uintptr_t)caller - (uintptr_t)info.dli_fbase;
-      printf("offset: %ld called by: %ld\n", offset, caller_offset);
+      fprintf(trace_fp, "offset: %ld called by: %ld\n", offset, caller_offset);
+      fflush(trace_fp);
     }
-    fflush(stdout);
 }
